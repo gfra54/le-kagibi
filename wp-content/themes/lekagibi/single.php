cfc3b0b84ad7cb0966214c29091a653e38b6987e
Gilles François <gilles@sofoot.com>
2016-03-07 16:26:25 +0100

    titre h1

--- wp-content/themes/lekagibi/single.php
+++ wp-content/themes/lekagibi/single.php
@@ -8,7 +8,7 @@ setup_postdata($post);
 <section id="<?php the_ID();?>" class="container content-section">
 	<div class="row">
 	<div class="col-lg-8 col-lg-offset-2">
-		<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 		<?php if(has_post_thumbnail()) {?>
 			<div class="entry-image"><?php the_post_thumbnail('large'); ?></div>
 		<?php }?>
